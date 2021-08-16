e54ebb414de0641711e039966ef476ee66ace39e
Cristhofer <cristhofert97@gmail.com>
2021-08-16 20:17:15 -0300

    first commit

---
+++ Practico1/Program.cs
@@ -0,0 +1,49 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practico1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Mi Programa en C#");
+            List<Persona> lista = new List<Persona>();
+            do
+            {
+                Persona per = new Persona();
+                try
+                {
+                    Console.Write("Nombre: ");
+                    per.Nombre = Console.ReadLine();
+
+                    Console.Write("Apellido: ");
+                    per.Apellido = Console.ReadLine();
+
+                    Console.Write("Documento de Identidad: ");
+                    per.Documento = Console.ReadLine();
+
+                    Console.Write("Fecha de Nacimiento: ");
+                    per.FechaNacimiento = Convert.ToDateTime(Console.ReadLine());
+
+                }
+                catch (Exception e) {
+                    Console.WriteLine("ERROR: " + e.Message);
+                }
+                lista.Add(per);
+
+                Console.WriteLine("exit para salir o cuialquier otra tecla para continuar: ");
+              
+            } while (!Console.ReadLine().Equals("exit"));
+
+            lista.ForEach(p =>
+            { Console.WriteLine(p.Nombre + " | " + p.Apellido + " | " + p.Documento + " | " + p.FechaNacimiento ); });
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Shared/Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+   
+
+    public class Persona
+    {
+        public String Nombre { get; set; }
+        public String Apellido { get; set; }
+
+        private string documento = "";
+        public String Documento {
+            get { return documento; }
+            set {
+                if (value is null || value.Length > 7)
+                    throw new Exception("Formato de documento incorrecto");
+                else
+                    documento = value.ToUpper();
+            }
+        }
+        public DateTime? FechaNacimiento { get; set; }
+    }
+}
